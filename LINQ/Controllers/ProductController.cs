08c647ea435f30fca5a4df6df50204e3195c06a4
haitqd00565 <haitqd00565@fpt.edu.vn>
2018-12-02 21:37:24 +0700

    basic linq

---
+++ LINQ/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LINQ.Context;
+using LINQ.Models;
+
+namespace LINQ.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductContext db = new ProductContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var product = db.Products.ToList();
+            return View(product);
+        }
+        public ActionResult OrderByName()
+        {
+            var products = from pro in db.Products orderby pro.Name ascending select pro;
+            return View(products);
+        }
+        public ActionResult OrderByPrice()
+        {
+            var products = from pro in db.Products orderby pro.Price ascending select pro;
+            return View(products);
+        }
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LINQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LINQ.Startup))]
+namespace LINQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
